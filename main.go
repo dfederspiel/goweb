d9b467b86c0a9b299ba9cd2d41956de6513f32e7
David Federspiel <david@federnet.com>
2020-01-30 02:10:49 -0500

    more cleanup on routes. added static file path for serving future react app and nested api in a "api" routing group

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
+	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"rsi.com/go-training/services"
 )
 
-/*
-* What is a package?
-* Wow, cool! So, then.. where are they how do I reference them?
-* Ahhh.. ok, yeah.. I think I get it. So why not just keep everything in main?
-* Sweet, what are good some good examples for how to organize all this stuff?
-* Solid!
- */
-
 func main() {
 	g := gin.Default()
-	services.RegisterRoutes(&g.RouterGroup)
+	g.Use(static.Serve("/", static.LocalFile("./build", true)))
+
+	api := g.Group("/api")
+	{
+		services.RegisterRoutes(api)
+	}
+
 	_ = g.Run()
 }
