92e22c1f4d40fa88b9b29c309da7ed42b70e5973
David Federspiel <david@federnet.com>
2020-01-30 01:39:58 -0500

    refactored and cleaned up code for review and preparing for database study

--- init.go
+++ init.go
@@ -9,22 +9,45 @@ import (
 
 /*StartServer sets up and runs our web server
  */
-func StartServer() {
-	r := gin.Default()
-	r.Routes()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, "pong")
-	})
-	r.POST("/ping", func(c *gin.Context) {
+func RegisterRoutes(router *gin.RouterGroup) {
+
+	router.GET("/animals", GetAnimals())
+	router.GET("/animal/:id", GetAnimal())
+	router.POST("/animal", CreateAnimal())
+	router.PUT("/animal/:id", UpdateAnimal())
+	router.DELETE("/animal/:id", DeleteAnimal())
+}
+
+func CreateAnimal() func(c *gin.Context) {
+	return func(c *gin.Context) {
 		var a services.Animal
-		c.BindJSON(&a)
-		fmt.Println(a)
-	})
-	r.PUT("/ping/:id", func(c *gin.Context) {
-		fmt.Println(c.Param("id"))
-	})
-	r.DELETE("/ping/:id", func(c *gin.Context) {
-		fmt.Println(c.Param("id"))
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+		_ = c.BindJSON(&a)
+
+		a.Name += " (Updated)"
+		c.JSON(200, a)
+	}
+}
+
+func GetAnimals() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		c.JSON(200, "Get  all animals")
+	}
+}
+
+func GetAnimal() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		_, _ = fmt.Fprintf(c.Writer, "Get animal %v", c.Param("id"))
+	}
+}
+
+func UpdateAnimal() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		_, _ = fmt.Fprintf(c.Writer, "Update animal %v", c.Param("id"))
+	}
+}
+
+func DeleteAnimal() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		_, _ = fmt.Fprintf(c.Writer, "Delete animal %v", c.Param("id"))
+	}
 }
